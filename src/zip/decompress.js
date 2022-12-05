f340a09cfc8c8060cf24b19fdad0f84722f19e4b
Anna Surahatava <anika3160@gmail.com>
2022-12-05 15:44:58 +0100

    feat: throw error is file not exist

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -2,27 +2,39 @@ import path from 'node:path';
 import fs from 'node:fs';
 import { fileURLToPath } from 'node:url';
 import zlib from 'node:zlib';
+import { promisify } from 'node:util';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const decompress = async () => {
     // Write your code here 
-    const readStream = fs.createReadStream(`${__dirname}/files/archive.gz`);
-    const writeStream = fs.createWriteStream(`${__dirname}/files/fileToCompress.txt`); 
-    const compressStream = zlib.createUnzip();
+    const pathToDecompressFile = `${__dirname}/files/fileToCompress.txt`;
+    const pathToArchive = `${__dirname}/files/archive.gz`;
+    const prAccess = promisify(fs.access);
 
-    const handleError = (err) => {
-        console.log(err)
-        readStream.destroy();
-        writeStream.end('Finished with error.')
+    try {
+        await prAccess(pathToArchive, fs.constants.F_OK);
+        const readStream = fs.createReadStream(pathToArchive);
+        const writeStream = fs.createWriteStream(pathToDecompressFile); 
+        const decompressStream = zlib.createUnzip();
+
+        const handleError = (err) => {
+            console.log(err);
+            readStream.destroy();
+            writeStream.end('Finished with error.');
+        }
+
+        readStream
+            .on('error', handleError)
+            .pipe(decompressStream)
+            .on('error', handleError)
+            .pipe(writeStream)
+            .on('error', handleError)
     }
-
-    readStream
-        .on('error', handleError)
-        .pipe(compressStream)
-        .pipe(writeStream)
-        .on('error', handleError)
+    catch (err) {
+        throw err
+    }
 };
 
 await decompress();
