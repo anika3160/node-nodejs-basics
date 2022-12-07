158c70ec4c1ac0d285513bd8be9cd2da9b49fed7
Anna Surahatava <anika3160@gmail.com>
2022-12-07 12:55:27 +0100

    feat: add print current path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import { stdin, stdout } from 'node:process'; 
+import { stdin, stdout } from 'node:process';
+// import path from 'node:path';
+// import { fileURLToPath } from 'node:url';
+import os from 'node:os';
 
 import { getValueByCLIArgs } from "./cli/args.js";
 import { FLAG_CONSTANTS} from "./constants.js";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
 
 const username = getValueByCLIArgs(FLAG_CONSTANTS.USERNAME_FLAG);
 
@@ -10,15 +17,20 @@ const printGoodbyeMsg = async () => {
     process.exit();
 } 
 
+const printCurrentDir = async (curDirname = os.homedir()) => {
+    stdout.write(`You are currently in ${curDirname}\n`);
+}
+
 const fileManager = async () => {
     stdout.write(`Welcome to the File Manager, ${username}!\n`);
+    process.on('SIGINT', printGoodbyeMsg);
+
     stdin.on('data', async (data) => {
         if (data.toString() === FLAG_CONSTANTS.EXIT_FLAG) {
            await printGoodbyeMsg();
         }
-    })
-    process.on('SIGINT', async () => {
-        await printGoodbyeMsg();
+
+        await printCurrentDir();
     })
 };
 
