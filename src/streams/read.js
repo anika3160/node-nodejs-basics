6be8afa0182cde8b488a8f91e018321f74e8cf38
Anna Surahatava <anika3160@gmail.com>
2025-04-27 23:57:34 +0200

    feat: add streams module

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,25 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
     // Write your code here 
+    const readStream = fs.createReadStream(`${__dirname}/files/fileToRead.txt`, {encoding:'utf-8'});
+
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    })
+
+    readStream.on('end', () => {
+        process.stdout.write('\n');
+    })
+
+    readStream.on('error', (err) => {
+        throw (err);
+    })
 };
 
 await read();
