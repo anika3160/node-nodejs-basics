b5f4ed7e3c8bf395678765fcf9710e498764345e
Anna Surahatava <anika3160@gmail.com>
2025-04-27 23:52:03 +0200

    feat: add cli module

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
     // Write your code here 
+    const arrayOfEntries = Object.entries(process.env);
+    const result = arrayOfEntries.reduce((acc, cur) => {
+        if (cur[0].indexOf('RSS_') === 0) {
+            return `${acc} ${cur[0]}=${cur[1]};`;
+        }
+        return acc
+    }, '')
+    console.log(result.slice(1, -1))
 };
 
 parseEnv();
