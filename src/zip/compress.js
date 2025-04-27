27f0a7a8cf6039ee3e02980ddba003ac39474319
Anna Surahatava <anika3160@gmail.com>
2025-04-27 23:58:38 +0200

    feat: add zlib module

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,40 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import zlib from 'node:zlib';
+import { promisify } from 'node:util';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
     // Write your code here 
+    const pathToCompressFile = `${__dirname}/files/fileToCompress.txt`;
+    const pathToArchive = `${__dirname}/files/archive.gz`;
+    const prAccess = promisify(fs.access);
+
+    try {
+        await prAccess(pathToCompressFile, fs.constants.F_OK);
+        const readStream = fs.createReadStream(pathToCompressFile, {encoding:'utf-8'});
+        const writeStream = fs.createWriteStream(pathToArchive);
+        const compressStream = zlib.createGzip();
+    
+        const handleError = (err) => {
+            console.log(err);
+            readStream.destroy();
+            writeStream.end('Finished with error.');
+        }
+    
+        readStream
+            .on('error', handleError)
+            .pipe(compressStream)
+            .on('error', handleError)
+            .pipe(writeStream)
+            .on('error', handleError)
+    }
+    catch (err) {
+        throw err
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,40 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import zlib from 'node:zlib';
+import { promisify } from 'node:util';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
     // Write your code here 
+    const pathToDecompressFile = `${__dirname}/files/fileToCompress.txt`;
+    const pathToArchive = `${__dirname}/files/archive.gz`;
+    const prAccess = promisify(fs.access);
+
+    try {
+        await prAccess(pathToArchive, fs.constants.F_OK);
+        const readStream = fs.createReadStream(pathToArchive);
+        const writeStream = fs.createWriteStream(pathToDecompressFile); 
+        const decompressStream = zlib.createUnzip();
+
+        const handleError = (err) => {
+            console.log(err);
+            readStream.destroy();
+            writeStream.end('Finished with error.');
+        }
+
+        readStream
+            .on('error', handleError)
+            .pipe(decompressStream)
+            .on('error', handleError)
+            .pipe(writeStream)
+            .on('error', handleError)
+    }
+    catch (err) {
+        throw err
+    }
 };
 
 await decompress();
