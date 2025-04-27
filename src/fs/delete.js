891c15534e80310b78552022e91678af15158697
Anna Surahatava <anika3160@gmail.com>
2025-04-27 23:43:26 +0200

    feat: add fs module

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { unlink } from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { promisify } from 'node:util';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
     // Write your code here 
+    const prUnlink = promisify(unlink);
+    const pathToDeleteFile = `${__dirname}/files/fileToRemove.txt`;
+    try {
+        await prUnlink(pathToDeleteFile)
+    }
+    catch (err) {
+        throw new Error('FS operation failed'); 
+    }
 };
 
 await remove();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { promisify } from 'node:util';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
     // Write your code here 
+    const pathToReadFile = `${__dirname}/files/fileToRead.txt`;
+    const prReadFile = promisify(fs.readFile);
+
+    try {
+        const data = await prReadFile(pathToReadFile, 'utf-8');
+        console.log(data);
+    }
+    catch(err) {
+        throw new Error('FS operation failed')
+    }
 };
 
 await read();
