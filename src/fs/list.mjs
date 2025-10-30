3a7d1792b39d37c95b3b86843e57e368614a5b5f
Anna Surahatava <anika3160@gmail.com>
2025-10-30 16:09:28 +0100

    fix: alphabetical order - issue

--- src/fs/list.mjs
+++ src/fs/list.mjs
@@ -15,38 +15,40 @@ export const getList = async (pathToDir) => {
 };
 
 export const printTable = async (pathToCurrentDir) => {
-    const list = await getList(pathToCurrentDir);
+  const list = await getList(pathToCurrentDir);
 
-    if (list.length > 0) {
-        function Item (name, type) {
-            this.Name = name;
-            this.Type = type;
+  if (list.length === 0) {
+    console.log('Directory is empty.');
+    return;
+  }
+
+  class Item {
+        constructor(name, type) {
+            this.Name = name
+            this.Type = type
         }
-    
-        const pairsList = await Promise.all(list.map(async (el) => {
-            const pathToEl = pathToCurrentDir + path.sep + el;
-            const typeOfFile = await getElTypeFromPath(pathToEl);
-            
-            return { name: el, type: typeOfFile }
-        }));
-
-        const dirFirstSortedList = pairsList.filter((pair) => pair.type).sort((a, b) => {
-            let aScore = a.type === DATA_TYPES.file ? 10 : 0;
-            let bScore = b.type === DATA_TYPES.file ? 10 : 0;
-            if (a.name > b.name) aScore +=1;
-            if (a.name < b.name) bScore -=1;
-            if (aScore > bScore) return 1;
-            if (aScore < bScore) return -1
-            return 0
-        });
-
-        const table = dirFirstSortedList.reduce((acc, cur, idx) => {
-            acc[idx] = new Item(cur.name, cur.type);
-            return acc;
-        }, {});
-
-        console.table(table);
-    } else {
-        console.log('Directory is empty.');
     }
-}
+
+  const pairsList = await Promise.all(
+    list.map(async (el) => {
+      const pathToEl = pathToCurrentDir + path.sep + el;
+      const typeOfFile = await getElTypeFromPath(pathToEl);
+
+      return { name: el, type: typeOfFile };
+    })
+  );
+
+  const dirFirstSortedList = pairsList
+    .filter((pair) => pair.type)
+    .sort((a, b) => {
+      if (a.type === b.type) return a.name.localeCompare(b.name);
+      return a.type === DATA_TYPES.directory ? -1 : 1;
+    });
+
+  const table = dirFirstSortedList.reduce((acc, cur, idx) => {
+    acc[idx] = new Item(cur.name, cur.type);
+    return acc;
+  }, {});
+
+  console.table(table);
+};
